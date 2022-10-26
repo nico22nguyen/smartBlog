6b8727e66842fb9182a6f67ef2ebbfdda51ebd4f
Nic <nico.nguyen22@gmail.com>
2022-10-26 13:56:24 -0500

    updated test route

--- backend/smartblog/views/test.py
+++ backend/smartblog/views/test.py
@@ -1,7 +1,9 @@
 from json import dumps
 
 from django.http import HttpResponse
-from ..utils import getBody
+from ..utils import getBody, connection
+
+cursor = connection.cursor()
 
 def test(request):
   if request.method == 'POST':
@@ -14,8 +16,27 @@ def test(request):
 
     return HttpResponse(dumps(resp_body), content_type='application/json')
   elif request.method == 'GET':
+    cursor.execute('SELECT * FROM user')
+
+    users = []
+    for (iduser, email, password, first_name, last_name) in cursor:
+      print({
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
+        'password': password,
+        'iduser': iduser
+      })
+      users.append({
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
+        'password': password,
+        'iduser': iduser
+      })
     resp_body = {
-      'testValue * 2': 69
+      'testValue * 2': 69,
+      'users': users
     }
 
     return HttpResponse(dumps(resp_body), content_type='application/json')
