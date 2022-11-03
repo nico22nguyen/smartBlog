90cfae44628f2c2cb18bee92e3239429378b5242
Nic <nico.nguyen22@gmail.com>
2022-11-03 13:54:28 -0500

    wrote auth route

--- backend/smartblog/views/auth.py
+++ backend/smartblog/views/auth.py
@@ -1,6 +1,5 @@
-from hashlib import sha256
 from django.http import HttpResponse
-from ..utils import getBody
+from ..utils import getBody, params_missing, connection, getHashedString
 
 def handler(request):
   if request.method != 'POST':
@@ -13,28 +12,37 @@ def handler(request):
   email = body['email']
   raw_password = body['password']
 
-  hashed_password = getHashedString(raw_password)
+  # ensure email and password are provided
+  if params_missing([email, raw_password]):
+    response = HttpResponse('Expected email and password in request body')
+    response.status_code = 400
 
+    return response
 
-  # Send cross check given password with password from database
-  # ------
-  # ------
+  # prevent sql injections
+  if ';' in email:
+    response = HttpResponse('Invalid email')
+    response.status_code = 400
 
-  correct_password = 'PASS FROM DB'
+    return response
 
-  if correct_password == hashed_password:
+  cursor = connection.cursor()
+  cursor.execute(f'SELECT password FROM user WHERE email = "{email}"')
+  result = cursor.fetchone()
+
+  # 404 if no user with provided email
+  if cursor.rowcount == 0:
+    response = HttpResponse('No user with email ' + email)
+    response.status_code = 404
+
+    return response
+  
+  correct_hash = result[0]
+  attempt_hash = getHashedString(raw_password)
+
+  if attempt_hash == correct_hash:
     return HttpResponse('Login Successful')
   else:
     response = HttpResponse('Incorrect Password')
     response.status_code = 403
     return response
-
-# takes in string and returns sha256 hash of string
-def getHashedString(input):
-  # create hash object and feed it the input string
-  hash_object = sha256()
-  buffer = input.encode('utf-8')
-  hash_object.update(buffer)
-
-  # read the hash state
-  return hash_object.hexdigest()
