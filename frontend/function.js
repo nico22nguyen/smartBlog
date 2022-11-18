045c6c06546b5d47303ae96decc13df13d824731
Nic <nico.nguyen22@gmail.com>
2022-11-17 19:23:01 -0600

    moved external css/js files to head

--- frontend/function.js
+++
@@ -1,9 +0,0 @@
-function enableEditMode() {
-    richTextField.document.designMode = "on";
-  }
-  function Edit(command) {
-    richTextField.document.execCommand(command, false, null);
-  }
-  function execVal(command, value) {
-    richTextField.document.execCommand(command, false, value);
-  }
